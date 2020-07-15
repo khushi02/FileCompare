5ec94b34239e0cebb31f2944eea3fe3538ba4f4a
khushi02 <k9wadhwa@gmail.com>
2020-07-14 17:43:50 -0700

    Create FileCompare.py

---
+++ FileCompare.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 15:55:35 2020
+
+@author: k9wad
+"""
+
+"""
+*can compare -> figure out if the two files are comparable
+cleanup -> clean data
+total -> return length
+total diff -> return total differences
+percent diff -> return percent different
+total higher -> return total higher values
+percent higher -> return percent higher values
+total lower -> return total lower values
+percent lower -> return percent lower values
+*first mean -> return mean of first file
+*second mean -> return mean of second file
+*diff mean -> return diff in 2 means
+*first std -> return standard deviation of first file
+*second std -> return standard deviation of second file
+*diff std -> return diff in 2 stds
+optional: min, max, 25%, 50%, 75%
+"""
+
+import pandas as pd
+import numpy as np
+
+def canCompare(file1, file2):
+    return file1.columns.equals(file2.columns)
+
+def firstMean(file1):
+    np.mean(file1, axis=0)
+    
+def secondMean(file2):
+    np.mean(file2, axis=0)
+    
+def diffMean(file1, file2):
+    # should this be absolute?
+    # should I create variables?
+    np.subtract(np.mean(file1, axis=0), np.mean(file2, axis=0))
+    
+def firstSTD(file1):
+    np.std(file1, axis=0)
+    
+def secondSTD(file2):
+    np.std(file2, axis=0)
+    
+def diffSTD(file1):
+    # should this be absolute?
+    # should I create variables?
+    np.subtract(np.std(file1, axis=0), np.std(file2, axis=0))
+    
+file1 = pd.read_csv(r'C:\Users\k9wad\Downloads\LI Base.csv')
+file2 = pd.read_csv(r'C:\Users\k9wad\Downloads\LI Test.csv')
+
+print(canCompare(file1, file2))
+print(np.subtract(np.std(file1, axis=0), np.std(file2, axis=0)))
+
+#print(len(file1))
+#print(file1.shape)
+#print(file1.head())
+#file1.info()
+#print(file1.columns)
+#print(file2.columns)
+#print("time_ms" in file1.keys())
+#print(file1.describe())
+#print(file1["time_ms"].max())
+#print(file1.loc["seq"].std())
+
